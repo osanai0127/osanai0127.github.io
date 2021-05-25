8ac9c8160ea6d09ef5501234f940371f3d717202
osanai0127 <82622792+osanai0127@users.noreply.github.com>
2021-05-25 10:30:27 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>テストファイルバージョン２</title>
+    </head>
+    <body>
+        <?php
+print "hello world";
+print "<br>";
+
+$a = mt_rand(1,5);
+if(3<$a){
+print "正解";
+};
+
+$week[]="月";
+$data = array("月","火","水");
+        ?>
+    </body>
+</html>
